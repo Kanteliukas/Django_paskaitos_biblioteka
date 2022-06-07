dbb9efedd96f98525d08be8d186bb26a69627489
infohata <infohata@gmail.com>
2022-06-07 21:06:05 +0300

    picture thumbnail; added admin link

--- mysite/user_profile/models.py
+++ mysite/user_profile/models.py
@@ -7,6 +7,7 @@ from django.contrib.auth import get_user_model
 # trečias variantas
 # from django.conf import settings
 # print(settings.AUTH_USER_MODEL)
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -24,6 +25,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{str(self.user)} {_("profile")}'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        picture = Image.open(self.picture.path)
+        if picture.height > 300 or picture.width > 300:
+            output_size = (300, 300)
+            picture.thumbnail(output_size)
+            picture.save(self.picture.path)
 
     class Meta:
         verbose_name = _("user profile")
