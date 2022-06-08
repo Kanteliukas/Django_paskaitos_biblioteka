b7f24ee17b591b1c9ef13f099b6e285b172787bf
infohata <infohata@gmail.com>
2022-06-08 18:58:44 +0300

    translation essentials

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -111,6 +112,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
+
+LANGUAGES = [
+    ('en-us', 'US English'),
+    ('lt', 'Lietuvių'),
+]
 
 TIME_ZONE = 'UTC'
 
